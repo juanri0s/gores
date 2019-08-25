756fe6d852cab4f3738c9152acadca765ec87b02
Juan Rios <juan.rios.28@gmail.com>
2019-08-25 17:18:33 -0400

    init commit. added cli package. added commands to handle cli params

---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"os"
+)
+
+var app = cli.NewApp()
+
+// TODO: add colors for logging
+
+func main() {
+	fmt.Println("Starting GoRes")
+
+	info()
+	commands()
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func info() {
+	app.Name = "GoRes"
+	app.Version = "1.0.0"
+	app.Author = "Juan Rios"
+	app.Email = "juansebrios@gmail.com"
+	app.Usage = "gores resume-section"
+}
+
+// TODO: fix types and create models? for them
+func contact() string {
+	// create kv pair of name, website, github, linkedin
+	return "Juan Rios"
+}
+
+func about() string {
+	// return quick description, maybe add flags for a short, medium, and long version
+	return "return"
+}
+
+func education() string {
+	// return kv pair of education including major, year graduated, GPA
+	return "return"
+}
+
+func skills() string {
+	// return kv pair of skills, years experience, and maybe a project that shows it
+	return "return"
+}
+
+func work() string {
+	// return kv pair of company, years, and bullet descriptions
+	return "return"
+}
+
+func projects() string {
+	// return kv pair of project, link, skills, year
+	return "return"
+}
+
+func publications() string {
+	// create kv of publication title, year, venue, authors
+	return "return"
+}
+
+func awards() string {
+	// create kv pair of award, year, company, description
+	return "return"
+}
+
+func download() string {
+	// download resume as pdf, might be hard to store on here so maybe we'll find a way to retrieve the latest
+	return "return"
+}
+
+func commands() {
+	app.Commands = []cli.Command{
+		{
+			Name:    "contact",
+			Aliases: []string{"c"},
+			Usage:   "Contact me",
+			Action: func(c *cli.Context) {
+				contact()
+			},
+		},
+		{
+			Name:    "about",
+			Aliases: []string{"ab"},
+			Usage:   "About me",
+			Action: func(c *cli.Context) {
+				about()
+			},
+		},
+		{
+			Name:    "education",
+			Aliases: []string{"e"},
+			Usage:   "View my education",
+			Action: func(c *cli.Context) {
+				education()
+			},
+		},
+		{
+			Name:    "skills",
+			Aliases: []string{"s"},
+			Usage:   "View my skills",
+			Action: func(c *cli.Context) {
+				skills()
+			},
+		},
+		{
+			Name:    "work",
+			Aliases: []string{"w"},
+			Usage:   "View my work experience",
+			Action: func(c *cli.Context) {
+				work()
+			},
+		},
+		{
+			Name:    "projects",
+			Aliases: []string{"p"},
+			Usage:   "View my projects",
+			Action: func(c *cli.Context) {
+				projects()
+			},
+		},
+		{
+			Name:    "publications",
+			Aliases: []string{"pubs"},
+			Usage:   "View my publications",
+			Action: func(c *cli.Context) {
+				publications()
+			},
+		},
+		{
+			Name:    "awards",
+			Aliases: []string{"a"},
+			Usage:   "View my awards",
+			Action: func(c *cli.Context) {
+				awards()
+			},
+		},
+		{
+			Name:    "pdf",
+			Aliases: []string{"pdf"},
+			Usage:   "Download resume as PDF",
+			Action: func(c *cli.Context) {
+				download()
+			},
+		},
+	}
+}
